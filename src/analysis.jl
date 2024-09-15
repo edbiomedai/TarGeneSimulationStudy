d4f206f4e241631e1a4403ea522f9b8c45bf7efe
Olivier Labayle <s2042526>
2024-09-15 15:04:51 +0100

    add dvc config

--- src/analysis.jl
+++ src/analysis.jl
@@ -26,4 +26,18 @@ function main()
     plot_coverage_by_positivity_across_estimators(constrained_estimands_coverages; sample_size=500_000)
     plot_coverage_by_positivity_across_estimators(constrained_estimands_coverages; sample_size=50_000)
 
+    # Realistic Simulation
+    ## Files
+    realistic_dataset_file = joinpath("null_simulation", "results", "datasets", "all_genotypes.data.arrow")
+    realistic_results_file = joinpath("null_simulation", "results", "null_simulation_results.hdf5")
+    ## Data Loading
+    realistic_dataset = Arrow.Table(realistic_dataset_file) |> DataFrame
+    realistic_results = jldopen(io -> io["results"], realistic_results_file)
+    ## Plots
+    add_estimator_info_cols!(realistic_results)
+    plot_coverage_per_estimator(realistic_results)
+    constrained_estimands_coverages = get_constrained_estimands_coverages(realistic_results, realistic_dataset)
+    plot_coverage_by_positivity_across_estimators(constrained_estimands_coverages; sample_size=500_000)
+    plot_coverage_by_positivity_across_estimators(constrained_estimands_coverages; sample_size=50_000)
+
 end
